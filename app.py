758757bd3b8d08af783d9822ea4f311d86e6953f
A7medGaber <www.a7medgaber12@gmail.com>
2023-12-05 16:04:53 +0200

    Students Done

--- app.py
+++ app.py
@@ -1,16 +1,88 @@
+# from flask import Flask, render_template, request, redirect, url_for
+# import sqlite3
 
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-from oop import Student 
+# <<<<<<< HEAD
+# from oop import Student
 
-import sys
-from oop import *
+# =======
+# import sys
+# from oop import *
+# >>>>>>> 01c4a63eaf7276403aad85219d0b79ec52caa6c4
+# app = Flask(__name__)
+# app.secret_key=['password']
+# # Set up SQLite database
+# DATABASE = 'database.db'
+
+# def create_table():
+#     connection = sqlite3.connect(DATABASE)
+#     cursor = connection.cursor()
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS students (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             first_name TEXT NOT NULL,
+#             middle_name TEXT,
+#             last_name TEXT NOT NULL,
+#             contact_number TEXT NOT NULL,
+#             national_id TEXT NOT NULL,
+#             email TEXT NOT NULL,
+#             date_of_birth TEXT NOT NULL,
+#             gender TEXT NOT NULL,
+#             class_level TEXT NOT NULL,
+#             password TEXT NOT NULL
+#         )
+#     ''')
+#     connection.commit()
+#     connection.close()
+
+# create_table()
+
+
+# @app.route("/")
+# def index():
+#     return render_template("sign_up_for_students.html")
+
+
+# @app.route('/sign_up_for_students', methods=['POST'])
+# def sign_up_for_students():
+#     s=Student()
+#     if request.method == 'POST':
+#         s.data = request.form
+#         connection = sqlite3.connect(DATABASE)
+#         cursor = connection.cursor()
+#         cursor.execute('''
+#             INSERT INTO students (
+#                 first_name, middle_name, last_name,
+#                 contact_number, national_id, email,
+#                 date_of_birth, gender, class_level, password
+#             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+#         ''', (
+#             s.data['first-name'],
+#             s.data['middle-name'],
+#             s.data['last-name'],
+#             s.data['contact-number'],
+#             s.data['national-id'],
+#             s.data['email'],
+#             s.data['date-of-birth'],
+#             s.data['gender'],
+#             s.data['class_level'],
+#             s.data['password']
+#         ))
+#         connection.commit()
+#         connection.close()
+#         return "<h1>Welcome <h1/>"
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+# app.py
+from flask import Flask, render_template,request
+import sqlite3 
+from oop import Student
 
 app = Flask(__name__)
-app.secret_key=['password']
-# Set up SQLite database
-DATABASE = 'database.db'
-
+DATABASE='database.db'
 def create_table():
     connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
@@ -34,13 +106,33 @@ def create_table():
 
 create_table()
 
-
 @app.route("/")
 def index():
-    return render_template("sign_up_for_students.html")
+    return render_template("index.html")
 
 
-@app.route('/sign_up_for_students', methods=['POST'])
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/log_in")
+def log_in():
+    return render_template("log_in.html")
+
+
+@app.route("/sign_up")
+def sign_up():
+    
+    return render_template("sign_up.html")
+
+
+@app.route("/sign_up_for_students",methods=['GET','POST'])
 def sign_up_for_students():
     s=Student()
     if request.method == 'POST':
@@ -67,40 +159,6 @@ def sign_up_for_students():
         ))
         connection.commit()
         connection.close()
-        return render_template("log_in.html")
-
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-
-@app.route("/log_in")
-def log_in():
-    return render_template("log_in.html")
-
-
-@app.route("/sign_up")
-def sign_up():
-    return render_template("sign_up.html")
-
-
-@app.route("/sign_up_for_students")
-def sign_up_for_students():
     return render_template("sign_up_for_students.html")
 
 
