fec6c13e2ec0e71926301b7f61252af7bbd213b9
A7medGaber <www.a7medgaber12@gmail.com>
2023-12-05 20:24:00 +0200

    Ass.Prof Table

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 from flask import Flask, render_template, request
 import sqlite3
-from oop import Student, Professor  # Assuming you have a class Professor in oop module
+from oop import Student, Professor,Professor_asst  # Assuming you have a class Professor in oop module
 
 app = Flask(__name__)
 DATABASE = 'database.db'
@@ -48,6 +48,25 @@ def create_table2():
     ''')
     connection.commit()
     connection.close()
+def create_table3():
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS assistant (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            middle_name TEXT,
+            last_name TEXT NOT NULL,
+            contact_number TEXT NOT NULL,
+            national_id TEXT NOT NULL,
+            email TEXT NOT NULL,
+            date_of_birth TEXT NOT NULL,
+            gender TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    connection.commit()
+    connection.close()
 
 def create_table_courses():
     connection = sqlite3.connect(DATABASE)
@@ -69,6 +88,7 @@ def create_table_courses():
 
 create_table()
 create_table2()
+create_table3()
 create_table_courses()
 
 
@@ -127,8 +147,32 @@ def sign_up_for_students():
     return render_template("sign_up_for_students.html")
 
 
-@app.route("/sign_up_for_ass_prof")
+@app.route("/sign_up_for_ass_prof",methods=['GET', 'POST'])
 def sign_up_for_ass_prof():
+    a=Professor_asst()
+    if request.method == 'POST':
+        a.data = request.form
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute('''
+            INSERT INTO assistant (
+                first_name, middle_name, last_name,
+                contact_number, national_id, email,
+                date_of_birth, gender, password
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            a.data['first-name'],
+            a.data['middle-name'],
+            a.data['last-name'],
+            a.data['contact-number'],
+            a.data['national-id'],
+            a.data['email'],
+            a.data['date-of-birth'],
+            a.data['gender'],
+            a.data['password']
+        ))
+        connection.commit()
+        connection.close()
     return render_template("sign_up_for_ass_prof.html")
 
 
