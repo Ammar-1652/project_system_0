9a60d488f54c1c1178bd6b848ab67bf50c1e2749
A7medGaber <www.a7medgaber12@gmail.com>
2023-12-05 18:10:34 +0200

    Semi DB

--- app.py
+++ app.py
@@ -1,88 +1,12 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import sqlite3
-
-# <<<<<<< HEAD
-# from oop import Student
-
-# =======
-# import sys
-# from oop import *
-# >>>>>>> 01c4a63eaf7276403aad85219d0b79ec52caa6c4
-# app = Flask(__name__)
-# app.secret_key=['password']
-# # Set up SQLite database
-# DATABASE = 'database.db'
-
-# def create_table():
-#     connection = sqlite3.connect(DATABASE)
-#     cursor = connection.cursor()
-#     cursor.execute('''
-#         CREATE TABLE IF NOT EXISTS students (
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             first_name TEXT NOT NULL,
-#             middle_name TEXT,
-#             last_name TEXT NOT NULL,
-#             contact_number TEXT NOT NULL,
-#             national_id TEXT NOT NULL,
-#             email TEXT NOT NULL,
-#             date_of_birth TEXT NOT NULL,
-#             gender TEXT NOT NULL,
-#             class_level TEXT NOT NULL,
-#             password TEXT NOT NULL
-#         )
-#     ''')
-#     connection.commit()
-#     connection.close()
-
-# create_table()
-
-
-# @app.route("/")
-# def index():
-#     return render_template("sign_up_for_students.html")
-
-
-# @app.route('/sign_up_for_students', methods=['POST'])
-# def sign_up_for_students():
-#     s=Student()
-#     if request.method == 'POST':
-#         s.data = request.form
-#         connection = sqlite3.connect(DATABASE)
-#         cursor = connection.cursor()
-#         cursor.execute('''
-#             INSERT INTO students (
-#                 first_name, middle_name, last_name,
-#                 contact_number, national_id, email,
-#                 date_of_birth, gender, class_level, password
-#             ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-#         ''', (
-#             s.data['first-name'],
-#             s.data['middle-name'],
-#             s.data['last-name'],
-#             s.data['contact-number'],
-#             s.data['national-id'],
-#             s.data['email'],
-#             s.data['date-of-birth'],
-#             s.data['gender'],
-#             s.data['class_level'],
-#             s.data['password']
-#         ))
-#         connection.commit()
-#         connection.close()
-#         return "<h1>Welcome <h1/>"
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
 # app.py
-from flask import Flask, render_template,request
-import sqlite3 
-from oop import Student
+from flask import Flask, render_template, request
+import sqlite3
+from oop import Student, Professor  # Assuming you have a class Professor in oop module
 
 app = Flask(__name__)
-DATABASE='database.db'
+DATABASE = 'database.db'
+
+
 def create_table():
     connection = sqlite3.connect(DATABASE)
     cursor = connection.cursor()
@@ -104,7 +28,49 @@ def create_table():
     connection.commit()
     connection.close()
 
+
+def create_table2():
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS profs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            middle_name TEXT,
+            last_name TEXT NOT NULL,
+            contact_number TEXT NOT NULL,
+            national_id TEXT NOT NULL,
+            email TEXT NOT NULL,
+            date_of_birth TEXT NOT NULL,
+            gender TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    connection.commit()
+    connection.close()
+
+def create_table_courses():
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+
+    # Example table creation
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS courses (
+            id INTEGER PRIMARY KEY,
+            course_name TEXT,
+            instructor TEXT,
+            start_date DATE,
+            end_date DATE
+        )
+    ''')
+
+    connection.commit()
+    connection.close()
+
 create_table()
+create_table2()
+create_table_courses()
+
 
 @app.route("/")
 def index():
@@ -128,13 +94,12 @@ def log_in():
 
 @app.route("/sign_up")
 def sign_up():
-    
     return render_template("sign_up.html")
 
 
-@app.route("/sign_up_for_students",methods=['GET','POST'])
+@app.route("/sign_up_for_students", methods=['GET', 'POST'])
 def sign_up_for_students():
-    s=Student()
+    s = Student()
     if request.method == 'POST':
         s.data = request.form
         connection = sqlite3.connect(DATABASE)
@@ -167,8 +132,32 @@ def sign_up_for_ass_prof():
     return render_template("sign_up_for_ass_prof.html")
 
 
-@app.route("/sign_up_for_prof")
+@app.route("/sign_up_for_prof", methods=['GET', 'POST'])
 def sign_up_for_prof():
+    p = Professor()
+    if request.method == 'POST':
+        p.data = request.form
+        connection = sqlite3.connect(DATABASE)
+        cursor = connection.cursor()
+        cursor.execute('''
+            INSERT INTO profs (
+                first_name, middle_name, last_name,
+                contact_number, national_id, email,
+                date_of_birth, gender, password
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            p.data['first-name'],
+            p.data['middle-name'],
+            p.data['last-name'],
+            p.data['contact-number'],
+            p.data['national-id'],
+            p.data['email'],
+            p.data['date-of-birth'],
+            p.data['gender'],
+            p.data['password']
+        ))
+        connection.commit()
+        connection.close()
     return render_template("sign_up_for_prof.html")
 
 
